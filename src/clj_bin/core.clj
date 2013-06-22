c2c254084980726a7759f7c33b9dbbc8202fd8ca
David Conner <dconner.pro@gmail.com>
2013-06-22 03:52:21 -0600

    core_test: added basic test for ser-intmsg

--- src/clj_bin/core.clj
+++ src/clj_bin/core.clj
@@ -1,6 +1,28 @@
-(ns clj-bin.core)
+(ns clj-bin.core
+   (:require [clojureweclj-bin.core-test :as :ser)))
 
 (defn foo
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+;;ccnb encoding => xml
+
+
+;;=============
+;; serializer
+;;=============
+(defn ser-intmsg
+  "Serializer - ccnb - interest message"
+)
+
+
+;;=============
+;; gloss
+;;=============
+
+
+
+;;=============
+;; vertigo
+;;=============
--- test/clj_bin/core_test.clj
+++ test/clj_bin/core_test.clj
@@ -1,7 +1,32 @@
 (ns clj-bin.core-test
   (:require [clojure.test :refer :all]
-            [clj-bin.core :refer :all]))
+            [clj-bin.core :refer :all])
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+;;=============
+;; serializer
+;;=============
+
+(deftest serializer-interest
+  (testing "Serializer - ccnb - interest message"
+    (is (= 
+      (clj-bin.core/ser-intmsg 16r01d2f2faa55041524300fa9d000102000000) (
+"<Interest>
+  <Name>
+    <Component ccnbencoding="text">PARC</Component>
+    <Component ccnbencoding="base64Binary">AAEC</Component>
+  </Name>
+</Interest>")))))
+
+;;=============
+;; gloss
+;;=============
+
+;;=============
+;; vertigo
+;;=============
+
+;; serializer
